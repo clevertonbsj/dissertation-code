3b97dc468110f1100f4130f04bbcd4bb97bbf3a1
clevertonbsj <cbsj052@hotmail.com>
2023-06-20 11:38:57 -0300

    finished the implemementation of the NN

--- mylib.py
+++ mylib.py
@@ -13,7 +13,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as func
 import torch.optim as optim
-import torch.autograd as autograd
 from torch.autograd import Variable
 
 def create_graph(warehouse_number, delivery_number):
@@ -589,12 +588,50 @@ class Dqn():
         self.last_bias = self.bias
         self.last_reward = 0
     
+    def create_bias(self, state):
+        probs = func.softmax(self.model(Variable(state, volatile = True)))
+        bias = probs.multinomial()
+        return bias
+    
     def learn(self, batch_state, batch_next_state, batch_reward, batch_bias):
         bias = self.model(batch_state).squeeze(1)
-        next_bias = self.model(batch_next_state).detach().max()[0] #needs to be revised for the problem
+        next_bias = self.model(batch_next_state).detach()
         tgt = self.gamma * next_bias + batch_reward
         td_loss = func.smooth_l1_loss(bias, tgt)
         self.optimizer.zero_grad()
         td_loss.backward(retain_variables = True)
         self.optimizer.step()
 
+    def update(self, reward, last_state):
+        new_state = torch.Tensor(last_state).float().unsqueeze(0)
+        self.memory.push((self.last_state, new_state, torch.Tensor(self.last_bias).unsqueze(0), torch.Tensor([self.last_reward])))
+        self.bias = self.create_bias(new_state)
+        if len(self.memory.memory) > 100:
+            batch_state, batch_next_state, batch_reward, batch_bias = self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_bias)
+        self.last_bias = self.bias
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+        if len(self.reward_window) > 1000:
+            del self.reward_window[0]
+        return self.bias
+    
+    def score(self):
+        return sum(self.reward_window)/(len(self.reward_window)+1.)
+
+    def save(self):
+        torch.save({'state_dict' : self.model.state_dict(), 
+                    'optimizer' : self.optimizer.state_dict()},
+                   'last_brain.pth')
+    
+    def load(self):
+        if os.path.isfile('last_brain.pth'):
+            print('Loading...')
+            checkpoint = torch.load('last_brain.pth')
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print('done')
+        else:
+            print('no save file found')
+        
